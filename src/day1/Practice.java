96f599c9340d2b097c38c49883779b4b889c1a1c
serifekilic <purdemirderya@yahoo.com>
2020-02-29 19:14:39 -0500

    New code

--- src/day1/Practice.java
+++ src/day1/Practice.java
@@ -2,6 +2,7 @@ package day1;
 
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("Thank you Furkan");
+        System.out.println("Thank you very much  Furkan");
+        System.out.println("I feel happy now");
     }
 }
