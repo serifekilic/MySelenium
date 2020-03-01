a080447003618f8672631e4b9a1fb025eab148d1
serifekilic <purdemirderya@yahoo.com>
2020-02-29 19:18:26 -0500

    first commit

--- src/day1/Practice.java
+++ src/day1/Practice.java
@@ -4,5 +4,6 @@ public class Practice {
     public static void main(String[] args) {
         System.out.println("Thank you very much  Furkan");
         System.out.println("I feel happy now");
+        System.out.println("I loved java and want to love Selenium");
     }
 }
